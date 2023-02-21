dc0959f8d00ca044aadda3bb20718a9e637d5796
ayako913 <121155701+ayako913@users.noreply.github.com>
2023-02-21 20:59:45 +0900

    userバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+  validates :name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :tel, presence: true,numericality: { only_integer: true, message:'is invalid. Input only number' }, format: { with: /\A0\d{9,10}\z/, message:'is too short'}
+  validates :division_id, numericality: { other_than: 1 } 
+  validates :city_area, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+       
 end
