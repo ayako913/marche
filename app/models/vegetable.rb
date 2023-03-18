3b852325da8b68a478f37e34656ed2a46cdfd26c
ayako913 <121155701+ayako913@users.noreply.github.com>
2023-03-18 10:39:45 +0900

    vegetableバリデーション設定

--- app/models/vegetable.rb
+++ app/models/vegetable.rb
@@ -4,6 +4,11 @@ class Vegetable < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
+  validates :vegetable, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+  validates :place_id, numericality: { other_than: 1 } 
+
   def self.search(search)
     if search != ""
       Vegetable.joins(:user).where('users.city_area LIKE(?)', "%#{search}%")
